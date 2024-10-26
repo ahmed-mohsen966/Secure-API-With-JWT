27275f318f09a22de5ad544d02b6754512d06978
ahmed-mohsen966 <ah.mohsen75@gmail.com>
2024-10-26 16:42:21 +0300

    Authenticate API using token generated by another system

---
+++ TestIntegration/Controllers/IntegrationController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TestIntegration.Models;
+
+namespace TestIntegration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IntegrationController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public IntegrationController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [Authorize]
+        [HttpGet("TestAuth")]
+        public IActionResult TestAuth()
+        {
+            var token = Request.Headers.Authorization.FirstOrDefault();
+            var userClaims = User.Claims.ToList();
+            return Ok("User authenticated");
+        }
+    }
+}
